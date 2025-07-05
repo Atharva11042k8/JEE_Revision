53db20d849c96745ed63b3fc3a780c4ff62ccd73
Atharva11042k8 <169069337+Atharva11042k8@users.noreply.github.com>
2025-07-05 23:17:23 +0530

    Update subjectConfig.js

--- src/data/subjectConfig.js
+++ src/data/subjectConfig.js
@@ -49,6 +49,7 @@ export const SUBJECT_CONFIG = [
       { name: 'Circles', file: 'mathematics/circles.json' },
       { name: 'Functions', file: 'mathematics/functions.json' },
       { name: 'Inverse Trig Functions', file: 'mathematics/itf.json' },
+      { name: 'parabola', file:'mathematics/parabola.json'},
     ]
   },
   {
